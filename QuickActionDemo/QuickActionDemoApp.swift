e191adc6383cdd962e3f1bfb5261c335385eba58
itsuki <itsuki.enjyoy@gmail.com>
2024-09-30 15:40:20 +0900

    Initial Commit

---
+++ QuickActionDemo/QuickActionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickActionDemoApp.swift
+//  QuickActionDemo
+//
+//  Created by Itsuki on 2024/09/30.
+//
+
+import SwiftUI
+
+@main
+struct QuickActionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
