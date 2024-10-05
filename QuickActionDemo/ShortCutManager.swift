378184734ba5a5290a7d6ac9db127d3acc7eb4e9
itsuki <itsuki.enjyoy@gmail.com>
2024-10-05 15:28:39 +0900

    demo for home screen quick actions.

---
+++ QuickActionDemo/ShortCutManager.swift
@@ -0,0 +1,24 @@
+//
+//  QuickActionManager.swift
+//  QuickActionDemo
+//
+//  Created by Itsuki on 2024/10/02.
+//
+
+import SwiftUI
+
+class QuickActionManager: ObservableObject {
+    static let instance = QuickActionManager()
+    
+    @Published var type: String?
+    
+    func 
+    
+    
+}
+
+
+enum QuickAction {
+    case unlike
+    case lastOpened(symbolName: String)
+}
